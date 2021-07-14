a9e1f223262a79e75763080897d4a2b59b5d9433
kjmatt91 <kjmatt91@gmail.com>
2021-07-13 23:12:40 -0400

    commented out some notes in orderBook.js to help breakdown what's needed in the assignment.

---
+++ orderBook.js
@@ -0,0 +1,14 @@
+// create a `reconcileOrder` function which will properly reconcile an incoming cryptocurrency order given the existing book of orders.
+function reconcileOrder()
+
+// if statements, buy or sell?
+
+// update quantities of matching prices 
+
+// objects in arrays - use dot notation to update quantity
+
+// at the end - return updated book in reconciled order (updated orders go to the end of the updated book)
+
+// if buy - it runs the buy order statement
+
+//  if sell - it runs the sell order statement
