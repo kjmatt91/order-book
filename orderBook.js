90e395d7fd1666fb81d64223cf6ff3a31af65dbc
kjmatt91 <kjmatt91@gmail.com>
2021-07-22 00:04:12 -0400

    Made some adjustments to functions, successfully passing first 3 tests

--- orderBook.js
+++ orderBook.js
@@ -1,27 +1,45 @@
 function reconcileOrder(existingBook, incomingOrder) {
   let updatedBook = []
 
-  if (existingBook.length <= 0) {
+  if (existingBook.length <= 0) {// works - don't change
     // console.log(updatedBook.concat(incomingOrder))
-    return updatedBook.concat(incomingOrder)
+    return existingBook.concat(incomingOrder)
   }
 
   for (let i = 0; i < existingBook.length; i++) {
-    if (incomingOrder.type !== existingBook[i].type && incomingOrder.price === existingBook[i].price) {
-      return true
+    if (incomingOrder.type === existingBook[i].type) {// works - don't change
+      existingBook.push(incomingOrder)
+
+      return existingBook
     }
-    if (incomingOrder.type === 'buy') {
-      let existingBook = existingBook.push(incomingOrder)
+
+    if ((incomingOrder.type !== existingBook[i].type) && (incomingOrder.price !== existingBook[i].price)) {// works - don't change
+      existingBook.push(incomingOrder)
+
+      return existingBook
     }
-    if (incomingOrder.type === 'sell' && incomingOrder.quantity >= existingBook[i].quantity &&
-    incomingOrder.price === existingBook[i].price) {
-      updatedBook.push(incomingOrder)
+    // eslint-disable-next-line max-len
+    if ((incomingOrder.type !== existingBook[i].type) && (incomingOrder.price >= existingBook[i].price) || (incomingOrder.price <= existingBook[i].price)) {
+      if (incomingOrder.type === 'buy') {
+        existingBook.push(incomingOrder)
+
+        return existingBook
+      }
+      if (incomingOrder.type === 'sell' && incomingOrder.quantity >= existingBook[i].quantity &&
+      incomingOrder.price === existingBook[i].price) {
+        updatedBook.push(incomingOrder)
+      }
+      // if (incomingOrder.quantity === existingBook[i].quantity && incomingOrder.price === existingBook[i].price) {
+      //   let existingBook = existingBook.splice(existingBook[i], 1)
+
+      //   i--
+      // }
+      if (existingBook.includes(incomingOrder)) {
+        return updatedBook.splice(existingBook[i])
+      }
     }
-    // if there's anything leftover on both orders  - both are added to book
-    // if there's anything leftover on one order - only that order gets added to the book 
-    // else - push existing book order in
     else {
-      updatedBook = existingBook.concat(incomingOrder)
+      return existingBook.concat(incomingOrder)
     }
   }
 
